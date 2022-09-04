d6f722a36dd722814488ba4d2a1a5bb18428e405
James Bhatta <jmsbhatta@gmail.com>
2022-09-04 16:22:42 +0545

    wrap routes with auth middleware and redirect home to login page

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 
-Route::get('racks', [RackController::class, 'index'])->name('racks.index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('racks', [RackController::class, 'index'])->name('racks.index');
 
-Route::get('files', [FileController::class, 'index'])->name('files.index');
-Route::get('files/create', [FileController::class, 'create'])->name('files.create');
-Route::post('files', [FileController::class, 'store'])->name('files.store');
-Route::get('files/{file}/edit', [FileController::class, 'edit'])->name('files.edit');
-Route::put('files/{file}', [FileController::class, 'update'])->name('files.update');
-Route::delete('files/{file}', [FileController::class, 'destroy'])->name('files.destroy');
+    Route::get('files', [FileController::class, 'index'])->name('files.index');
+    Route::get('files/create', [FileController::class, 'create'])->name('files.create');
+    Route::post('files', [FileController::class, 'store'])->name('files.store');
+    Route::get('files/{file}/edit', [FileController::class, 'edit'])->name('files.edit');
+    Route::put('files/{file}', [FileController::class, 'update'])->name('files.update');
+    Route::delete('files/{file}', [FileController::class, 'destroy'])->name('files.destroy');
+});
